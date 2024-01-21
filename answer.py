c1435560cf9f96682d5fb8f0c15dbfbc654ca661
mpotturi1 <157025789+mpotturi1@users.noreply.github.com>
2024-01-21 22:18:47 +0530

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,17 +1,20 @@
 def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
+    """Prints 'Hello, World!'
+        This function prints the given message
     """
-
-    """
-
     # Write a comment; comments are very useful to other programmers.
-
+    #This statement prints 'Hello,World'
     # Write a Python statement to print Hello, World!
 
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
+hello_world()
+
+result = add(4,2)
+print(result)
